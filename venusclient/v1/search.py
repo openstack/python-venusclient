13e259fc6c5493ce1ad309d4a07811617f33f14c
jiasirui <jiasirui3@163.com>
2022-09-12 16:19:37 +0800

    add interface of search log program
    Change-Id: Ia56d98df8ed6f962915dcfa2307e4f16b493f221

--- venusclient/v1/search.py
+++ venusclient/v1/search.py
@@ -77,6 +77,20 @@ class SearchManager(basemodels.BaseModelManager):
         url = '/v1/search/params'
 
         params = {
+            'type': "module_name",
+        }
+        url += utils.prepare_query_string(params)
+
+        try:
+            resp, body = self.api.json_request('GET', url)
+            return body
+        except Exception as e:
+            raise RuntimeError(str(e))
+
+    def get_type_program(self, args):
+        url = '/v1/search/params'
+
+        params = {
             'type': "program_name",
         }
         url += utils.prepare_query_string(params)
