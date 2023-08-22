971f4a70ef547bf33d46861f4986cf3be3036702
leiyuehui <leiyuehui-s@inspur.com>
2023-08-22 21:32:26 +0800

    Add AnomalyManager to venusclient
    Change-Id: I2f2c6e94ba5be089b2089272b4f25df9d9f3cc6f

--- venusclient/v1/client.py
+++ venusclient/v1/client.py
@@ -18,6 +18,7 @@ from keystoneauth1 import session as ksa_session
 from oslo_utils import importutils
 from venusclient.common import httpclient
 from venusclient.v1 import analyse
+from venusclient.v1 import anomaly
 from venusclient.v1 import config
 from venusclient.v1 import search
 
@@ -184,6 +185,7 @@ class Client(object):
         self.config = config.ConfigManager(self.http_client)
         self.search = search.SearchManager(self.http_client)
         self.analyse = analyse.AnalyseManager(self.http_client)
+        self.anomaly = anomaly.AnomalyManager(self.http_client)
 
         profile = kwargs.pop("profile", None)
         if profiler and profile:
