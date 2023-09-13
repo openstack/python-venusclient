8d6b6f6ecd07a712c55e089df2cd10263c6a085f
leiyashuai <leiyashuai@inspur.com>
2023-09-13 19:16:52 +0800

    Add set-custom-config subcommand to venusclient
    Change-Id: I64fb1aad787e2c95536ec563bde91ac9233f5568

--- venusclient/v1/config_shell.py
+++ venusclient/v1/config_shell.py
@@ -12,9 +12,24 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from venusclient.common import cliutils as utils
+
 
 def do_get_log_storage_days(cs, args):
     """get the days of saved logs in elasticsearch(unit day)."""
     endpoint = cs.config.get_days()
+    print(endpoint)
+    return endpoint
+
+
+@utils.arg('id',
+           metavar='<id>',
+           help='The id of custom config')
+@utils.arg('value',
+           metavar='<value>',
+           help='The value set to custom config')
+def do_set_custom_config(cs, args):
+    """set the custom config."""
+    endpoint = cs.config.set_custom_config(args.id, args.value)
     print(endpoint)
     return endpoint
